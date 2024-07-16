102107cdbe11a39cfab31e98c06ea5f3e3ccb5ae
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-07-16 19:12:24 -0300

    tasks: create routes and controllers to create, edit, delete and return tasks

--- cs50_django/settings.py
+++ cs50_django/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'newyear',
+    'tasks',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
